b7783e05705480c82ed8d5884bb22b263739c621
Brian Houston <>
2023-05-01 11:30:50 -0400

    file for load words

--- verba/cli.py
+++ verba/cli.py
@@ -1,22 +1,13 @@
 import csv
 from importlib.resources import files
 import verba.word.endings as endings
+import verba.word.load_words as load_words
 import verba.input as input
 from verba.word.noun import Noun
 
-def get_words(library_name):
-    words = []
-
-    with files('verba.data').joinpath(library_name).joinpath('nouns.tsv').open(encoding='utf-16') as file:
-        tsv_file = csv.DictReader(file, delimiter='\t')
-        for line in tsv_file:
-            words.append(Noun(line))
-
-    return words
 
 def main():
-    endings.load_endings()
-    get_words('LL')
+    load_words.load_words('LL')
     print(endings.endings)
     word = input.read_latin_input()
     print(word)
---
+++ verba/word/load_words.py
@@ -0,0 +1,13 @@
+import csv
+from importlib.resources import files
+from verba.word.noun import Noun
+
+def load_words(library_name):
+    words = []
+
+    with files('verba.data').joinpath(library_name).joinpath('nouns.tsv').open(encoding='utf-16') as file:
+        tsv_file = csv.DictReader(file, delimiter='\t')
+        for line in tsv_file:
+            words.append(Noun(line))
+
+    return words
