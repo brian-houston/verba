641fb824c5a2ca0b66b90c4d3ffb944c39fd496f
Brian Houston <brianhxenon@gmail.com>
2023-05-02 21:07:25 -0400

    lookup tables for ordering

--- verba/word/definitions.py
+++ verba/word/definitions.py
@@ -12,3 +12,37 @@ inflections_lookup = {
                 'declension': 'base',
             }
         }
+
+attributes_order = {
+        'noun': ['gender', 'case', 'number'],
+        'adjective': ['gender', 'case', 'number'],
+        'verb': ['mood', 'tense', 'voice', 'person', 'number']
+        'participle': ['tense', 'voice', 'gender', 'case', 'number'],
+        }
+
+value_to_attributes = {
+            'f': 'gender',
+            'm': 'gender',
+            'n': 'gender',
+            'nom': 'case',
+            'acc': 'case',
+            'gen': 'case',
+            'dat': 'case',
+            'abl': 'case',
+            's': 'number',
+            'p': 'number',
+            'pres': 'tense',
+            'fut': 'tense',
+            'imperf': 'tense',
+            'perf': 'tense',
+            'futp': 'tense',
+            'plup': 'tense',
+            'ind': 'mood',
+            'impera': 'mood',
+            'subj': 'mood',
+            '1st': 'person',
+            '2nd': 'person',
+            '3rd': 'person',
+            'act': 'voice',
+            'pas': 'voice',
+        }
