7f9fb3e5b7b557d8c10a068730979c93dca1af0f
Brian Houston <brianhxenon@gmail.com>
2023-05-13 16:35:50 -0400

    words selected using weights

--- verba/question/utils.py
+++ verba/question/utils.py
@@ -1,21 +1,30 @@
 import random
 
+# factor a word's weight is multiplied by if it is chosen
+falloff = 0.33
+
 def inflection_generator(words, inflection_keys):
     fails = 0
+    weights = [1 for _ in words]
+    n_words = len(words)
     while True:
-        if fails > 10:
+        # stop if all weights are zero
+        # or words is empty
+        if fails == n_words:
             return StopIteration()
 
-        word = random.choice(words)
+        i = random.choices(range(n_words), weights=weights)[0]
+        word = words[i]
+
         possible_inflection_keys = set(inflection_keys).intersection(word.get_inflection_keys())
         possible_inflection_keys = list(possible_inflection_keys)
 
         if not possible_inflection_keys:
             fails += 1
+            weights[i] = 0 
             continue
-        else: 
-            fails = 0
 
+        weights[i] *= falloff 
         selected_key = random.choice(possible_inflection_keys)
 
         yield (word, selected_key)
